dd22f1cfca1d7d3e6e5c475bfc886c6910493c8d
Nia Charrington <nia@charrington.com>
2019-12-01 07:58:38 -0800

    updating cleaning and batching script

--- data/scripts/clean_batch_manifest.py
+++ data/scripts/clean_batch_manifest.py
@@ -1,4 +1,35 @@
+import argparse
+import pickle
+import json
+
 parser = argparse.ArgumentParser(
     description='Reduce fasta file to common sequences.')
 parser.add_argument('input_file', type=str, help='fasta file')
 args = parser.parse_args()
+
+with open(args.input_file, 'r') as data_file:
+    json_data = data_file.read()
+
+data = json.loads(json_data)
+
+output_list = []
+BATCH_SIZE = 500
+file_suffix = 1
+for record in data:
+    if record['scientific_name'] == 'human gut metagenome':  # picking only fecal samples
+        temp_dict = {}
+        sample_accession = record['sample_accession']
+        temp_dict['sample_accession'] = sample_accession
+        temp_dict['xml_ftp'] = f'https://www.ebi.ac.uk/ena/browser/api/xml/{sample_accession}?download=true'
+        temp_dict['fastq_ftp'] = record['fastq_ftp']
+        output_list.append(temp_dict)
+        if len(output_list) == BATCH_SIZE:  # dividing into batches of 500
+            with open(f'manifest_batch{file_suffix}.pickle', 'wb') as filehandle:
+                pickle.dump(output_list, filehandle)
+                filehandle.close()
+            output_list = []
+            file_suffix += 1
+
+with open(f'manifest_batch{file_suffix}.pickle', 'wb') as filehandle:
+    pickle.dump(output_list, filehandle)
+    filehandle.close()
