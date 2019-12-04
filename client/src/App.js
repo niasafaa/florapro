44e28bb9ac5b2474e3c28af949de7f1d9ecd3647
Nia Charrington <nia@charrington.com>
2019-12-04 07:01:04 -0800

    load landing page from root

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const register = lazy(() => import('./components/register'));
 const login = lazy(() => import('./components/login'));
-const protectedTEST = lazy(() => import('./components/protectedTEST'));
+const protectedDash = lazy(() => import('./components/protectedDash'));
 const landingPage = lazy(() => import('./components/landingPage'));
 const dashBoard = lazy(() => import('./components/dashboard/Dashboard.js'));
 
@@ -13,34 +13,11 @@ const App = () => {
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <React.Fragment>
-              <h1>FloraPRO</h1>
-              <Link style={{ display: 'flex' }} to="/register">
-                Register
-              </Link>
-              <Link style={{ display: 'flex' }} to="/login">
-                Login
-              </Link>
-              <Link style={{ display: 'flex' }} to="/protectedTEST">
-                Protected
-              </Link>
-              <Link style={{ display: 'flex' }} to="/landingPage">
-                Landing Page
-              </Link>
-              <Link style={{ display: 'flex' }} to="/dashBoard">
-                Dasboard
-              </Link>
-            </React.Fragment>
-          )}
-        />
+        <Route exact path="/" render={() => <Redirect to="/landingPage" />} />
         <Route exact path="/landingPage" component={landingPage} />
         <Route exact path="/register" component={register} />
         <Route exact path="/login" component={login} />
-        <Route exact path="/protectedtest" component={protectedTEST} />
+        <Route exact path="/protecteddash" component={protectedDash} />
         <Route exact path="/dashBoard" component={dashBoard} />
       </Suspense>
     </Router>
