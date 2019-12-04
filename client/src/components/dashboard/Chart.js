eebe65f590d0b299f5088b0e543965da390ef348
Nia Charrington <nia@charrington.com>
2019-12-04 06:58:14 -0800

    major changes to dashboard

--- client/src/components/dashboard/Chart.js
+++ client/src/components/dashboard/Chart.js
@@ -1,46 +1,47 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer
+} from 'recharts';
 import Title from './Title';
 
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
-}
-
 const data = [
-  createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
-  createData('24:00', undefined),
+  { Species: 'P. weihenstes', Reads: 853 },
+  { Species: 'E. marmotae', Reads: 625 },
+  { Species: 'B. uniformis', Reads: 151 },
+  { Species: 'B. timonensis', Reads: 532 },
+  { Species: 'B. fragilis', Reads: 366 },
+  { Species: 'P. caspiana', Reads: 231 },
+  { Species: 'P. johnsonii', Reads: 65 },
+  { Species: 'D. simplex', Reads: 300 },
+  { Species: 'E. coli', Reads: 52 },
+  { Species: 'L. muris', Reads: 94 },
+  { Species: 'B. vulgatus', Reads: 46 }
 ];
 
 export default function Chart() {
   return (
     <React.Fragment>
-      <Title>Today</Title>
+      <Title>My Results</Title>
       <ResponsiveContainer>
-        <LineChart
+        <BarChart
           data={data}
           margin={{
             top: 16,
             right: 16,
-            bottom: 0,
-            left: 24,
+            bottom: 50,
+            left: 24
           }}
         >
-          <XAxis dataKey="time" />
-          <YAxis>
-            <Label angle={270} position="left" style={{ textAnchor: 'middle' }}>
-              Sales ($)
-            </Label>
-          </YAxis>
-          <Line type="monotone" dataKey="amount" stroke="#556CD6" dot={false} />
-        </LineChart>
+          <XAxis interval={0} dataKey="Species" angle={-45} textAnchor="end" />
+          <YAxis></YAxis>
+          <Tooltip />
+          <Bar dataKey="Reads" fill="#3257a8" />
+        </BarChart>
       </ResponsiveContainer>
     </React.Fragment>
   );
